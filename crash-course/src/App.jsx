7f0dc8d441c11b2adfeee7c917043cac0cf8da37
Joe <josephlewismoulton@gmail.com>
2022-10-25 15:12:57 +0100

    Components

---
+++ crash-course/src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import Todo from "./components/Todo";
+import Title from "./components/Title";
+import Modal from "./components/Modal"
+
+function App() {
+  return (
+    <div>
+      <Title></Title>
+      <div className="todo__wrapper">
+
+      <Todo></Todo>
+      <Todo></Todo>
+      <Todo></Todo>
+      </div>
+      <Modal></Modal>
+    </div>
+  );
+}
+
+export default App;
---
+++ crash-course/src/components/Todo.jsx
@@ -0,0 +1,13 @@
+
+import './Todo.css'
+
+function Todo() {
+  return (
+    <div className="todo">
+      <h2>Finish Frontend simplified</h2>
+      <button>Delete</button>
+    </div>
+  );
+}
+
+export default Todo;

eb129349e844df34b9c6aa80485a7529ac8a2cb8
Joe <josephlewismoulton@gmail.com>
2022-10-27 20:22:03 +0100

    Link Routing

--- crash-course/src/App.jsx
+++ crash-course/src/App.jsx
@@ -1,14 +1,21 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home.jsx";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Nav from "./components/Nav";
+
 
 
 function App() {
   return (
     <>
       <Router>
+      <Nav/>
         <Routes>
-          <Route path="/" element= {<Home></Home>}></Route>
+          <Route path="/" element= {<Home/>}></Route>
+          <Route path="/About" element= {<About/>}></Route>
+          <Route path="/Contact" element= {<Contact/>}></Route>
         </Routes>
       </Router>
     </>
